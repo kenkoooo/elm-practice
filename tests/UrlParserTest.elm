d82e59e6bede4a8ed9075f476dcc06e1c60fb022
kenkoooo <kenkou.n@gmail.com>
2019-08-02 03:07:40 +0900

    Fixed tests

--- tests/UrlParserTest.elm
+++ tests/UrlParserTest.elm
@@ -13,15 +13,15 @@ suite =
             \_ ->
                 "https://atcoder.jp/contests/tkppc4-1/tasks/tkppc4_1_h"
                     |> UrlParser.parseUrl
-                    |> Expect.equal (AtCoder "tkppc4-1" "tkppc4_1_h")
+                    |> Expect.equal (Just (AtCoder "tkppc4-1" "tkppc4_1_h"))
         , test "Parse URLs of Aizu Online Judge" <|
             \_ ->
                 "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=3022"
                     |> UrlParser.parseUrl
-                    |> Expect.equal (Aizu (Just 3022))
+                    |> Expect.equal (Just (Aizu (Just 3022)))
         , test "Parse URLs of others" <|
             \_ ->
                 "http://example.com/"
                     |> UrlParser.parseUrl
-                    |> Expect.equal (Other "http://example.com/")
+                    |> Expect.equal (Just (Other "http://example.com/"))
         ]
